79387008c7ceed2b015589cb7d11c987bcc31a40
kessahoud-art <kessahoud@gmail.com>
2026-03-22 09:13:54 +0100

    Update and rename config.js to js/config.js

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@
 // ============================================================
 
 window.__FA_SUPABASE_URL      = 'https://crfmdpvxiabwlhgrwcro.supabase.co';
-window.__FA_SUPABASE_ANON_KEY = 'VOTRE_ANON_KEY';
+window.__FA_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyZm1kcHZ4aWFid2xoZ3J3Y3JvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNDkxODgsImV4cCI6MjA4OTYyNTE4OH0.oHb2qnCArSkY_YNatrWuHLeWDS3WqvV0gpsnZuQmZRw';
 // ↑ Remplace VOTRE_ANON_KEY par ta vraie clé anon Supabase
